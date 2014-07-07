a6ae313e3f7178207b9f0caa4e6dc92400cb480a
Elias Rodrigues <elias19r@gmail.com>
2014-07-07 20:17:36 -0300

    Pipeline funcionando

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
 
 #define PROMPT "\\m/:"
 
-int go_on = 1; /* This variable controls the main loop. */
+int go_on = 1;			/* This variable controls the main loop. */
 
 /*
 	Built in function: print help info
@@ -67,7 +67,7 @@ int exec_builtin(char *cmd, char **cmdargv)
 		return 1;
 	}
 	if (strcmp(cmd, "exit") == 0 || strcmp(cmd, "quit") == 0) {
-		printf("nutshell says: GG\n"); /* Good game, bro (y) */
+		printf("nutshell says: GG\n");	/* Good game, bro (y) */
 		sleep(1);
 		go_on = 0;
 		return 1;
@@ -91,53 +91,50 @@ int exec_builtin(char *cmd, char **cmdargv)
 }
 
 /*
-	Executa a pipeline of commands
+	Execute a pipeline of commands
 */
-void execute_pipeline(pipeline_t *pipeline, int pipeA[2], int index)
+void execute_pipeline(pipeline_t * pipeline, int pipeA[2], int index)
 {
-	int pid, status, pipeB[2], ret;
-	
+	int pid, pipeB[2], ret;
+
 	if (index < 0)
 		return;
 
-	ret = pipe(pipeB);
-	fatal(ret < 0, "Could not create pipe.");
+	if (index > 0) {
+		ret = pipe(pipeB);
+		fatal(ret < 0, "Could not create pipe.");
+	}
 
 	pid = fork();
 	fatal(pid < 0, "Could not create process");
 
 	/* Parent: exec */
 	if (pid > 0) {
-		wait(&status);
 
-	} else { /* Child. */
-
-		if (index == pipeline->ncommands-1) {
+		/* Not last, redirect input */
+		if (index > 0) {
 			close(0);
 			dup(pipeB[0]);
 			close(pipeB[0]);
-			
-		} else if (index == 0) {
-			close(1);
-			dup(pipeA[1]);
-			close(pipeA[1]);
-			
-			/* Not used. */
-			close(pipeB[0]);
-			close(pipeB[1]);
-			
-		} else {
-			close(0);
-			dup(pipeB[0]);
-			close(pipeB[0]);			
-			
+		}
+
+		/* Not first, redirect output */
+		if (index < pipeline->ncommands - 1) {
 			close(1);
 			dup(pipeA[1]);
 			close(pipeA[1]);
 		}
 
-		execute_pipeline(pipeline, pipeB, index-1);
+		/* Not used. */
+		close(pipeB[1]);
+
+		/* Run command */
 		execvp(pipeline->command[index][0], pipeline->command[index]);
+
+		/* Child: recurse */
+	} else {
+		execute_pipeline(pipeline, pipeB, index - 1);
+		return;
 	}
 }
 
@@ -163,52 +160,74 @@ int main(int argc, char **argv)
 		fatal(ret < 0, NULL);
 
 		/* Parse command line. */
-
 		if (parse_command_line(command_line, pipeline) == 0) {
 
 			if (pipeline->ncommands < 1)
 				continue;
-	  
+
 			/* Try to execute built in command */
-			if (exec_builtin(pipeline->command[0][0], pipeline->command[0]))
+			if (exec_builtin
+			    (pipeline->command[0][0], pipeline->command[0]))
 				continue;
-			
+
 			/* Running group of process in foreground */
 			if (RUN_FOREGROUND(pipeline)) {
 
 				/* More than one command */
 				if (pipeline->ncommands > 1) {
-					execute_pipeline(pipeline, NULL, pipeline->ncommands-1);
-
-
-				/* Only one command was read */
-				} else {
 
 					/* Create another process and check for fatal error */
 					pid = fork();
-					fatal(pid < 0, "Could not create a new process.");
-				
+					fatal(pid < 0,
+					      "Could not create a new process.");
+
 					/* If parent  */
 					if (pid > 0) {
 						/* Just wait for child to end */
 						wait(&status);
-				
-					/* If child */
+
+						/* If child */
+					} else {
+						execute_pipeline(pipeline, NULL,
+								 pipeline->
+								 ncommands - 1);
+						return EXIT_SUCCESS;
+					}
+
+					/* Only one command was read */
+				} else {
+
+					/* Create another process and check for fatal error */
+					pid = fork();
+					fatal(pid < 0,
+					      "Could not create a new process.");
+
+					/* If parent  */
+					if (pid > 0) {
+						/* Just wait for child to end */
+						wait(&status);
+
+						/* If child */
 					} else {
 
 						/* Redirecting input */
 						if (REDIRECT_STDIN(pipeline)) {
-							tmp = open(pipeline->file_in, O_RDONLY);
-							fatal(tmp < 0, "Could not redirect input: file not found");
+							tmp =
+							    open(pipeline->
+								 file_in,
+								 O_RDONLY);
+							fatal(tmp < 0,
+							      "Could not redirect input: file not found");
 
 							/* Close file descriptor 0 which is 'stdin' */
 							close(0);
 
 							/* Make a copy of file descriptor opened for input,
-								thus now it is associated with file descriptor 0 that
-								has been just released */
+							   thus now it is associated with file descriptor 0 that
+							   has been just released */
 							fd = dup(tmp);
-							fatal(fd < 0, "Could not redirect input.");
+							fatal(fd < 0,
+							      "Could not redirect input.");
 							close(tmp);
 						}
 
@@ -216,40 +235,69 @@ int main(int argc, char **argv)
 						if (REDIRECT_STDOUT(pipeline)) {
 							/* '>' Overwrite if file exists, or create a new one */
 							if (REDIRECT_STDOUT_WRITE(pipeline)) {
-								tmp = open(pipeline->file_out, O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
-								fatal(tmp < 0, "Could not redirect output: failed to create a file");
+								tmp =
+								    open
+								    (pipeline->
+								     file_out,
+								     O_CREAT |
+								     O_TRUNC |
+								     O_RDWR,
+								     S_IRUSR |
+								     S_IWUSR);
+								fatal(tmp < 0,
+								      "Could not redirect output: failed to create a file");
 
-							/* '>>' Append to existing file */
-							} else if (REDIRECT_STDOUT_APPEND(pipeline)) {
-								tmp = open(pipeline->file_out, O_CREAT | O_APPEND | O_RDWR, S_IRUSR | S_IWUSR);
-								fatal(tmp < 0, "Could not redirect output: failed to append to file");
+								/* '>>' Append to existing file */
+							} else
+							    if
+							    (REDIRECT_STDOUT_APPEND
+							     (pipeline)) {
+								tmp =
+								    open
+								    (pipeline->
+								     file_out,
+								     O_CREAT |
+								     O_APPEND |
+								     O_RDWR,
+								     S_IRUSR |
+								     S_IWUSR);
+								fatal(tmp < 0,
+								      "Could not redirect output: failed to append to file");
 							}
 
 							/* Close file descriptor 1 which is 'stdout' */
 							close(1);
 
 							/* Make a copy of file descriptor opened for output,
-								thus now it is associated with file descriptor 1 that
-								has been just released */
+							   thus now it is associated with file descriptor 1 that
+							   has been just released */
 							fd = dup(tmp);
-							fatal(fd < 0, "Could not redirect input.");
+							fatal(fd < 0,
+							      "Could not redirect input.");
 							close(tmp);
 						}
 
 						/* Execute a program */
-						execvp(pipeline->command[0][0], pipeline->command[0]);
+						execvp(pipeline->command[0][0],
+						       pipeline->command[0]);
 
 						/* If program not found */
-						printf("%s: command not found\n", pipeline->command[0][0]);
+						printf
+						    ("%s: command not found\n",
+						     pipeline->command[0][0]);
 						printf("nutshell says: QQ\n");
+
+						return EXIT_FAILURE;
 					}
 				}
 
-			/* Running group of process in background */
+				/* Running group of process in background */
 			} else {
 				if (REDIRECT_STDIN(pipeline)) {
+					/* TODO */
 				}
 				if (REDIRECT_STDOUT(pipeline)) {
+					/* TODO */
 				}
 			}
 		}
@@ -260,4 +308,3 @@ int main(int argc, char **argv)
 
 	return EXIT_SUCCESS;
 }
-
